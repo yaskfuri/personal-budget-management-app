77e34d9768e52dc75cfe6a5a52e955263380ed30
yaskfuri <y.abhraokfuri@student.fontys.nl>
2024-12-06 14:26:49 +0100

    front-end and backend integration
    with axios

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ import sqlite3
 # App + Flask API with jsonify 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return jsonify({"message": "hellow world!"})
 
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-CORS(app)
+
 
 
 # Database connection with SQLite
